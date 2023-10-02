8110774800a5bae04482c6a0be8ac48ae143d6c2
root <root@Rafael-Merlotto>
2023-10-02 10:23:24 +0200

    created client and config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+import * as _ from 'ts-loader';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
